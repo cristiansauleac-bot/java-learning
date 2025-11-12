4c372c8196f17ca43ea65cb256a0c44659b56dee
Cristian <cristian.sauleac@gmail.com>
2025-11-12 13:32:37 +0200

    Main

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
 
-        int a = 4;
-        String b = (a==3) ? "TRUE" : "FALSE";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(b);
+            System.out.print("Enter first number: ");
+            double first = scanner.nextDouble();
+            System.out.print("Enter the operator (+ - * /): ");
+            char operator = scanner.next().charAt(0);
+            System.out.print("Enter second number: ");
+            double second = scanner.nextDouble();
+
+        double result;
+        switch (operator) {
+            case '+': result = first + second; break;
+            case '-': result = first - second; break;
+            case '*': result = first * second; break;
+            case '/':
+                if (second == 0) {
+                    System.out.print("Cannot divide by 0\n");
+                    scanner.close();
+                    return;
+                }
+                result = first / second; break;
+            default:
+                System.out.print("Invalid operator\n");
+                scanner.close();
+                return;
+        }
+
+        System.out.print("Result: "+result);
+
+        scanner.close();
 
     }
 }
